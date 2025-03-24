4f5ff122f6d5528643325443657de4f3882ecb33
mhmd <mhmdlotdy368@gmail.com>
2025-03-24 12:34:52 +0200

    add constance

---
+++ lib/Core/Utiles/constans.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constans {
+  static const String baseUrl = 'https://vcare.integration25.com/api';
+  static const String hiveSearch = 'hiveSearch';
+  static const String hiveAppointment = 'hiveAppointment';
+}
