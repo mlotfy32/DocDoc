e2669fa7c9df19fa46b9a700a05d2fd2c204fd1f
mhmd <mhmdlotdy368@gmail.com>
2025-03-24 12:32:38 +0200

    add styles

---
+++ lib/Core/Utiles/style/font_Styles/app_fonts.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+enum lang { en, ar }
+
+class AppFonts {
+  static TextStyle f32w700 = TextStyle(
+    fontSize: 32.sp,
+    color: const Color(0xff247CFF),
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle f10w400 = TextStyle(
+    color: const Color(0xff757575),
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle f16w600 = TextStyle(
+    color: Colors.white,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle f24w700 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xff247CFF),
+  );
+  static TextStyle f14w400 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xff757575),
+  );
+  static TextStyle hintStyle = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xffC2C2C2),
+  );
+  static TextStyle f12w400 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xff247CFF),
+  );
+  static TextStyle f13w400 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xff242424),
+  );
+  static TextStyle f11w400 = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xff9E9E9E),
+  );
+  static TextStyle f18w500 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xff242424),
+  );
+  static TextStyle f18w600 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+  static TextStyle f18w700 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xff242424),
+  );
+  static TextStyle f20w700 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xff242424),
+  );
+  static TextStyle f19w500 = TextStyle(
+      fontSize: 18.sp,
+      fontWeight: FontWeight.w500,
+      color: const Color(0xff247CFF));
+}
