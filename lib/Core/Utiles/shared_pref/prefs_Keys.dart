465cfa0e104e8a9aa0d2a442b3f95742589e6b65
mhmd <mhmdlotdy368@gmail.com>
2025-03-24 12:37:27 +0200

    add sharedPrefs

---
+++ lib/Core/Utiles/shared_pref/prefs_Keys.dart
@@ -0,0 +1,12 @@
+class PrefsKeys {
+  static const String rememberMe = "rememberMe";
+  static const String accessToken = "accessToken";
+  static const String id = "id";
+  static const String userImage = "userImage";
+  static const String hasFaceId = "hasFaceId";
+  static const String userName = "userName";
+  static const String email = "email";
+  static const String pass = "pass";
+  static const String phone = "phone";
+  static const String language = "language";
+}
